0ae3b45ee8fcef28b3596fc5b0734bb55eaa07c8
Geniece Tims <geniece.c.tims@gmail.com>
2023-07-05 11:27:08 -0500

    currently working on deleteContact function

--- src/ContactsList.java
+++ src/ContactsList.java
@@ -6,156 +6,119 @@ import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class ContactsList {
-private static Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
 
-public static void showContactList(Path datafile) throws IOException {
-    List<String> lines = Files.readAllLines(datafile);
-    for(int i = 0; i < lines.size(); i += 1) {
+
+    public static void showContactList(Path datafile) throws IOException {
+        List<String> lines = Files.readAllLines(datafile);
+        for (int i = 0; i < lines.size(); i += 1) {
             System.out.println((i + 1) + ": " + lines.get(i));
         }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-public static void AddContact() throws IOException {
-    System.out.println("Enter First and Last Name. ");
-    String nameInput = sc.nextLine();
-    System.out.println("Enter phone number.");
-    String numberInput = sc.nextLine();
-    String contactInfo = nameInput + " - " + numberInput;
-    System.out.println("Arrays.asList(contactInfo) = " + Arrays.asList(contactInfo));
-    Files.write(
-            Paths.get("data", "contacts.txt"),
-            Arrays.asList(contactInfo),
-            StandardOpenOption.APPEND
-    );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// THROWS IOException:
-public static void runApp () throws IOException{
-    String directory = "data";
-    String filename = "contacts.txt";
-
-    Path dataDirectory = Paths.get(directory);
-    Path dataFile = Paths.get(directory, filename);
-
-    if (Files.notExists(dataDirectory)) {
-        Files.createDirectories(dataDirectory);
-    }
-
-    if (!Files.exists(dataFile)) {
-        Files.createFile(dataFile);
-    }
-
-    List<String> contacts = Arrays.asList(
-            "Jerrod Cooper - 754-244-7389",
-            "Elizabeth Taylor - 816-544-6492",
-            "Harry Potter - 302-190-7349",
-            "Olivia Spencer - 305-559-1412",
-            "Jerry Springer - 757-468-1112",
-            "Idris Elba - 212-444-2323",
-            "Beyonce Knowles - 281-503-7262",
-            "Michael Jones - 281-330-8004",
-            "Soulja Boi - 678-999-8212",
-            "Little Wayne - 504-981-2617"
-    );
-    Files.write(dataFile, contacts);
-
-    System.out.println("Choose from the following menu:");
-    System.out.println(" Press 1, to view contacts \n Press 2, to add contact\n Press 3, to search contacts");
-    int userAnswer = sc.nextInt();
-
-    if(userAnswer == 1){
-        showContactList(dataFile);
-    } else if (userAnswer == 2) {
-        AddContact();
     }
 
 
+    public static void AddContact() throws IOException {
+        System.out.println("Enter First and Last Name. ");
+        String nameInput = sc.nextLine();
+        System.out.println("Enter phone number.");
+        String numberInput = sc.nextLine();
+        String contactInfo = nameInput + " - " + numberInput;
+        System.out.println("Arrays.asList(contactInfo) = " + Arrays.asList(contactInfo));
+        Files.write(
+                Paths.get("data", "contacts.txt"),
+                Arrays.asList(contactInfo),
+                StandardOpenOption.APPEND
+        );
+    }
+
+
+    public static void searchContact(Scanner scanner) {
+
+    }
+
+
+    public static void deleteContact(Scanner sc) {
+        System.out.print("Enter the name to delete : ");
+        String name = sc.nextLine();
+
+
+    }
+
+
+    public static void exitList () {
 
 }
 
 
+    // THROWS IOException:
+    public static void runApp() throws IOException {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (!Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        List<String> contacts = Arrays.asList(
+                "Jerrod Cooper - 754-244-7389",
+                "Elizabeth Taylor - 816-544-6492",
+                "Harry Potter - 302-190-7349",
+                "Olivia Spencer - 305-559-1412",
+                "Jerry Springer - 757-468-1112",
+                "Idris Elba - 212-444-2323",
+                "Beyonce Knowles - 281-503-7262",
+                "Michael Jones - 281-330-8004",
+                "Soulja Boi - 678-999-8212",
+                "Little Wayne - 504-981-2617"
+        );
+        Files.write(dataFile, contacts);
+
+        System.out.println("Choose from the following menu:");
+        System.out.println(" Press 1: View Contacts \n Press 2: Add Contact \n Press 3: Search Contacts \n Press 4: Delete Contact \n Press 5: Exit");
+        int userAnswer = sc.nextInt();
+
+        if (userAnswer == 1) {
+            showContactList(dataFile);
+        } else if (userAnswer == 2) {
+            AddContact();
+//        } else if (userAnswer == 3) {
+//            searchContact();
+        } else if (userAnswer == 4) {
+            deleteContact(sc);
+        } else if (userAnswer == 5) {
+            exitList();
+        }
 
 
+    }
 
 
+        public static void main (String[]args) throws IOException {
+            runApp();
 
 
+        }
 
+    }
 
-//private static void ReplaceContact() throws IOException {
-//    List<String> lines = Files.readAllLines(newContactstList);
-//    List<String> newContactsList = new ArrayList<>();
-//    for (String line : lines) {
-//        if (line.equals("Olivia Spencer - 305-559-1412")) {
-//            newContactsList.add("Angela Bassett - 555-555-5555");
-//            continue;
-//        }
-//        newContactsList.add(line);
-//    }
-//    Files.write(Paths.get("data", "contacts.txt"), newContactsList);
+//List<String> list = new ArrayList<>();
 //
-//    for(int i = 0; i < newContactsList.size(); i += 1) {
-//        System.out.println((i + 1) + ": " + newContactsList.get(i));
-//    }
-//}
-
-
-
-
-
-    public static void main(String[] args) throws IOException {
-        runApp();
-
-
-// May not need becuzwe are doing it on lines   Path dataDirectory = Paths.get(directory);
-//        Path dataFile = Paths.get(directory, filename);
-//        Path contactsPath = Paths.get("data", "contacts.txt");
-
-
-//        Files.write(
-//                Paths.get("data", "contacts.txt"),
-//                Arrays.asList("test"),
-//                StandardOpenOption.APPEND
-//        );
-//      Potentiallt deleteing later bcuz we have the add =contact method already.
-//        List<String> lines = Files.readAllLines(dataFile);
-//        List<String> newContactsList = new ArrayList<>();
-//        for (String line : lines) {
-//            if (line.equals("Olivia Spencer - 305-559-1412")) {
-//                newContactsList.add("Angela Bassett - 555-555-5555");
-//                continue;
-//            }
-//            newContactsList.add(line);
-//        }
-//        Files.write(Paths.get("data", "contacts.txt"), newContactsList);
-//            ReplaceContact();
-//        for(int i = 0; i < newContactsList.size(); i += 1) {
-//            System.out.println((i + 1) + ": " + newContactsList.get(i));
-//        }
-
-
-    }
-
-}
+//list.add("A");
+//        list.add("B");
+//        list.add("C");
+//        list.add("C");
+//        list.add("B");
+//        list.add("A");
+//
+//        System.out.println(list);
+//
+//        String removedStr = list.remove(1);
+//        System.out.println(list);
+//        System.out.println(removedStr);
