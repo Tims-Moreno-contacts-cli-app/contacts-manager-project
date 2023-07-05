729cb81bd39489016462597b0cf2a42818fb07cf
Michaella Moreno <michaella.moreno@gmail.com>
2023-07-05 16:57:35 -0400

    created new nethods and re arranged code for readability

--- src/ContactsList.java
+++ src/ContactsList.java
@@ -6,24 +6,27 @@ import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class ContactsList {
+//    OUR SCANNER
     private static Scanner sc = new Scanner(System.in);
 
-
+//USED TO DISPLAY THE LIST OF CONTACTS FROM OUR FILE. IT READS EVERY LINE FROM THE FILE AND PRINTS EACH LINE.
     public static void showContactList(Path datafile) throws IOException {
         List<String> lines = Files.readAllLines(datafile);
+        System.out.println("Name\t| Phone Number");
+        System.out.println("--------------------------");
         for (int i = 0; i < lines.size(); i += 1) {
             System.out.println((i + 1) + ": " + lines.get(i));
         }
     }
 
-
+//ALLOWS A CONTACT TO BE ADDED,
 // THROWS IOException:
     public static void AddContact() throws IOException {
         System.out.println("Enter First and Last Name. ");
         String nameInput = sc.nextLine();
         System.out.println("Enter phone number.");
         String numberInput = sc.nextLine();
-        String contactInfo = nameInput + " - " + numberInput;
+        String contactInfo = nameInput + " | " + numberInput;
         System.out.println("Arrays.asList(contactInfo) = " + Arrays.asList(contactInfo));
         Files.write(
                 Paths.get("data", "contacts.txt"),
@@ -33,21 +36,59 @@ public class ContactsList {
     }
 
 
-    public static void searchContact(Scanner scanner) {
+    public static void searchContact(Scanner sc) throws IOException {
+        System.out.println("Enter name: ");
+          String search = ContactsList.sc.next();
+        Path datafile = Paths.get("data", "contacts.txt");
+        List<String> lines = Files.readAllLines(datafile);
+        List<String> searchResults = new ArrayList<>();
+        for (String line : lines){
+            if (line.toLowerCase().contains(search.toLowerCase())){
+                searchResults.add(line);
+            }
+        }
 
+        if(searchResults.isEmpty()){
+            System.out.println("Contact not found");
+        } else {
+            System.out.println("Search results:");
+            for(String result: searchResults){
+                System.out.println(result);
+            }
+        }
     }
 
 
     public static void deleteContact(Scanner sc) {
         System.out.print("Enter the name to delete : ");
+//        sc.nextLine();
         String name = sc.nextLine();
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(Paths.get("data", "contacts.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> newList = new ArrayList<>();
+
+        for (String line : lines) {
+            if (line.contains(name)) {
+                continue;
+            }
+            newList.add(line);
+        }
+
+        try {
+            Files.write(Paths.get("data", "contacts.txt"), newList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
 
-
-    public static void exitList () {
-
+    public static void exitList (int status) {
+        Runtime.getRuntime().exit(status);
 }
 
 
@@ -67,38 +108,41 @@ public class ContactsList {
             Files.createFile(dataFile);
         }
 
-        List<String> contacts = Arrays.asList(
-                "Jerrod Cooper - 754-244-7389",
-                "Elizabeth Taylor - 816-544-6492",
-                "Harry Potter - 302-190-7349",
-                "Olivia Spencer - 305-559-1412",
-                "Jerry Springer - 757-468-1112",
-                "Idris Elba - 212-444-2323",
-                "Beyonce Knowles - 281-503-7262",
-                "Michael Jones - 281-330-8004",
-                "Soulja Boi - 678-999-8212",
-                "Little Wayne - 504-981-2617"
-        );
-        Files.write(dataFile, contacts);
 
-        System.out.println("Choose from the following menu:");
-        System.out.println(" Press 1: View Contacts \n Press 2: Add Contact \n Press 3: Search Contacts \n Press 4: Delete Contact \n Press 5: Exit");
-        int userAnswer = sc.nextInt();
+//        List<String> contacts = Arrays.asList(
+//                "Jerrod Cooper | 754-244-7389",
+//                "Elizabeth Taylor | 816-544-6492",
+//                "Harry Potter | 302-190-7349",
+//                "Olivia Spencer | 305-559-1412",
+//                "Jerry Springer | 757-468-1112",
+//                "Idris Elba | 212-444-2323",
+//                "Beyonce Knowles | 281-503-7262",
+//                "Michael Jones | 281-330-8004",
+//                "Soulja Boi | 678-999-8212",
+//                "Little Wayne | 504-981-2617"
+//        );
+//        Files.write(dataFile, contacts);
+        do {
+            System.out.println("Choose from the following menu:");
+            System.out.println(" Press 1: View Contacts \n Press 2: Add Contact \n Press 3: Search Contacts \n Press 4: Delete Contact \n Press 5: Exit");
+            int userAnswer = sc.nextInt();
+            sc.nextLine();
+                if (userAnswer == 1) {
+                    showContactList(dataFile);
+                } else if (userAnswer == 2) {
+                    AddContact();
+                } else if (userAnswer == 3) {
+                    searchContact(sc);
+                } else if (userAnswer == 4) {
+                    deleteContact(sc);
 
-        if (userAnswer == 1) {
-            showContactList(dataFile);
-        } else if (userAnswer == 2) {
-            AddContact();
-//        } else if (userAnswer == 3) {
-//            searchContact();
-        } else if (userAnswer == 4) {
-            deleteContact(sc);
-        } else if (userAnswer == 5) {
-            exitList();
-        }
-
-
+                } else if (userAnswer == 5) {
+                    exitList(0);
+                    break;
+            }
+        } while (true) ;
     }
+
 
 
         public static void main (String[]args) throws IOException {
@@ -109,17 +153,3 @@ public class ContactsList {
 
     }
 
-//List<String> list = new ArrayList<>();
-//
-//list.add("A");
-//        list.add("B");
-//        list.add("C");
-//        list.add("C");
-//        list.add("B");
-//        list.add("A");
-//
-//        System.out.println(list);
-//
-//        String removedStr = list.remove(1);
-//        System.out.println(list);
-//        System.out.println(removedStr);
